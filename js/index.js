4bc0cfcc960500a5559fc6106fb543f45066884a
FabianoEtec <nbscbs@gmail.com>
2021-10-26 01:03:10 -0300

    Finalizado IMC

--- js/index.js
+++ js/index.js
@@ -11,19 +11,24 @@ function mostrarError(){
         'Conforme suas informações'+ '</p>' + '<h1>' +'<span>' +'Seu IMC: ' + '</span>' + imc.toFixed(2) +  '</h1>' ;
        
     }if (imc < 17){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Muito abaixo do peso' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Muito abaixo do peso' + '</h2>'
+        + '<p>'+ 'Procure por um médico - ' + '<span>'+'  URGENTE.' +'</span>' + '</p>';
     }else if (imc >= 17 || imc <=18.49){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Abaixo do peso' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Abaixo do peso' + '</h2>'
+        + '<p>'+'Procure por um médico ' + '</p>';
     }if (imc >= 18.50 || imc >= 24.99){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Parabens! '+ '</span>' + ' Peso normal' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Parabens! '+ '</span>' + ' Peso normal' + '</h2>';
     }if (imc >= 25 || imc >= 29.99){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Acima do peso' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Acima do peso' + '</h2>';
     }if (imc >= 30 || imc >= 34.99){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade I' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade I' + '</h2>' 
+        + '<p>'+ 'Procure por um médico ' + '</p>';
     }if (imc >= 35 || imc >= 39.99){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade II (severa)' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade II (severa)' + '</h2>' + '<p>'+
+        'Procure por um médico - ' + '<span>'+'  URGENTE.' +'</span>' + '</p>';
     }if (imc > 40){
-        document.querySelector('#box3').innerHTML = '<h1>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade III (mórbida)' + '</h1>';
+        document.querySelector('#box3').innerHTML = '<h2>' + '<span>' + 'Alerta! '+ '</span>' + ' Obesidade III (mórbida)' + '</h2>' + '<p>'+
+        'Procure por um médico - ' + '<span>'+'  URGENTE.' +'</span>' + '</p>';
     }
     
 }
